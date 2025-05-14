7db5f563bfb061928e7599b476ad5689f58bd6c2
leonardomog2@gmail.com <leonardomog2@gmail.com>
2025-05-14 16:38:07 -0300

    (leo) Continuei mexendo no GerenciadorEmprestimos, criando o construtor e fazendo que ele so possa se acessado somente uma vez, tambem coloque um array para armazenar os livros emprestados e as datas que eles foram emprestados

--- services/GerenciadorUsuarios.java
+++ services/GerenciadorUsuarios.java
@@ -2,14 +2,14 @@ package services;
 import models.*;
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class GerenciadorUsuarios {
-    private static int qtdLivros=0;
     private ArrayList<Usuario> nomes;
     private Scanner leitor;
     private static GerenciadorUsuarios instancia;
-    
+
     private GerenciadorUsuarios(){
-        nomes = new ArrayList<>();
+        nomes = new ArrayList<Usuario>();
         leitor = new Scanner(System.in);
         System.out.println("SE QUISER PARAR DEIXE NOME VAZIO.");
         
@@ -29,16 +29,18 @@ public class GerenciadorUsuarios {
             System.out.println("tipo: ");
             String aux_tipo = leitor.nextLine();
             nomes.add(new Usuario(aux_nome,aux_cpf,aux_email,aux_telefone,aux_tipo));
-            qtdLivros ++;
         }   
     }
-
 
     public static GerenciadorUsuarios getInstancia(){
         if (instancia == null){
             instancia = new GerenciadorUsuarios();
         }
         return instancia;
+    }
+
+    public int NumeroUsuarios(){
+        return nomes.size();
     }
 
     public void UsuariosCadastrados(){
@@ -49,7 +51,6 @@ public class GerenciadorUsuarios {
 
     public void AdicionarUsuario(){
         System.out.println("SE QUISER PARAR DEIXE NOME VAZIO.");
-        
         while(true){
             System.out.println("----------------------------------");
             System.out.println("nome: ");
@@ -66,7 +67,6 @@ public class GerenciadorUsuarios {
             System.out.println("tipo: ");
             String aux_tipo = leitor.nextLine();
             nomes.add(new Usuario(aux_nome,aux_cpf,aux_email,aux_telefone,aux_tipo));
-            qtdLivros ++;
         }   
     }
     }
--- services/Gerenciadoremprestimos.java
+++ services/Gerenciadoremprestimos.java
@@ -3,20 +3,59 @@ import models.*;
 import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GerenciadorEmprestimos{
+    private ArrayList<Livro> livrosEmprestados;
+    private ArrayList<Date> listaDatas;
+    private Scanner ler;
     private Date dataEmprestimo;
     private SimpleDateFormat formato;
     private Usuario usuario;
     private Livro livro;
+    private static GerenciadorEmprestimos instancia;
     
-    
-    public GerenciadorEmprestimos(Usuario usuario, Livro livro, Date dataEmprestimo) throws ParseException {
+    private GerenciadorEmprestimos(Usuario usuario, Livro livro, Date dataEmprestimo) throws ParseException {
         formato = new SimpleDateFormat("dd/MM/yyyy");
+        livrosEmprestados = new ArrayList<Livro>();
+        listaDatas = new ArrayList<Date>();
 
         this.dataEmprestimo = formato.parse(formato.format(dataEmprestimo));
         this.usuario = usuario;
         this. livro = livro;
+        livrosEmprestados.add(livro);
+        listaDatas.add(dataEmprestimo);
+    }
+
+    public static GerenciadorEmprestimos getInstancia(Usuario usuario,Livro livro,Date dataEmprestimo) throws ParseException{
+        if (instancia == null){
+            instancia = new GerenciadorEmprestimos(usuario, livro, dataEmprestimo);
+        }
+        return instancia;
+        
+    }
+    public void AdicionarEmprestimo() throws ParseException{
+        ler = new Scanner(System.in);
+        formato = new SimpleDateFormat("dd/MM/yyyy");
+
+        while (true) {
+            String titulo = ler.nextLine();
+
+            String autor = ler.nextLine();
+            int numeroDePaginas = ler.nextInt();
+            String isbn = ler.nextLine();
+            livrosEmprestados.add(new Livro(titulo,autor,numeroDePaginas,isbn));
+
+            int dia = ler.nextInt();
+            int mes = ler.nextInt();
+            int ano = ler.nextInt() - 1900;
+
+            dataEmprestimo = new Date(ano,mes,dia);
+            this.dataEmprestimo = formato.parse(formato.format(dataEmprestimo));
+            listaDatas.add(dataEmprestimo);
+   
+        }
     }
 
     public void  MotrarDados(){
